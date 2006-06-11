58bcd852464cf8982dcedd00cfe51aaf39ab8143
rtogni <rtogni@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-06-11 19:28:46 +0000

    MPlayer1.0pre8
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/branches/1.0pre8_branch@18681 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-
-revision=r`grep committed-rev .svn/entries | sed 1q | cut -d '"' -f 2 2>/dev/null`
-
-extra=""
-if test "$1" ; then
-  extra="-$1"
-fi
-
-echo "#define VERSION \"dev-SVN-${revision}${extra}\"" > version.h
+echo "#define VERSION \"1.0pre8-$1\"" > version.h
