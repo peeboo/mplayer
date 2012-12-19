peeboo/mplayer

commit 1024be27e7a487bee2cd2031a196216eb09ea5ea

Don't overwrite video size information.
The information about the just now played video shall remain.

Reported by Hans-Dieter Kosch, hdkosch kabelbw de.


git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35572 b3059339-0415-0410-9bf9-f77b7e298cf2

gui/interface.c
@@ -816,8 +816,6 @@ int gui(int what, void *data)
 
             if (gtkShowVideoWindow) {
                 guiInfo.VideoWindow = True;
-                guiInfo.VideoWidth  = guiApp.video.width;
-                guiInfo.VideoHeight = guiApp.video.height;
 
                 if (!guiApp.videoWindow.isFullScreen) {
                     wsResizeWindow(&guiApp.videoWindow, guiApp.video.width, guiApp.video.height);

commit 4fd736295644967f215631c99fa4667b4b992bf2

Fix bug with wrong video window size after end of fullscreen playback.
(This is a regression introduced in r35572.)

The normal size the window should have (media size or configured size)
depends on the current state of playback. In order to influence the
size, add a boolean parameter. This usually is True unless playback has
stopped.


git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35699 b3059339-0415-0410-9bf9-f77b7e298cf2

gui/dialog/menu.c
@@ -573,7 +573,7 @@ GtkWidget * create_PopUpMenu( void )
 //   SubMenu=AddSubMenu( Menu,MSGTR_MENU_Size );
 //    AddMenuItem( SubMenu,MSGTR_MENU_NormalSize"      ", evNormalSize );
 //    AddMenuItem( SubMenu,MSGTR_MENU_DoubleSize, evDoubleSize );
-//    AddMenuItem( SubMenu,MSGTR_MENU_FullScreen, evFullScreen );
+//    AddMenuItem( SubMenu,MSGTR_MENU_FullScreen, evFullScreen + ( True << 16 ) );
 
 //  if ( guiInfo.Playing )
    {
@@ -659,7 +659,7 @@ GtkWidget * create_PopUpMenu( void )
     H=AddMenuCheckItem( window1, (const char*)half_xpm, Menu,MSGTR_MENU_HalfSize,b_half,evHalfSize );
     N=AddMenuCheckItem( window1, (const char*)normal_xpm, Menu,MSGTR_MENU_NormalSize"      ",b1,evNormalSize );
     D=AddMenuCheckItem( window1, (const char*)double_xpm, Menu,MSGTR_MENU_DoubleSize,b2,evDoubleSize );
-    F=AddMenuCheckItem( window1, (const char*)full_xpm, Menu,MSGTR_MENU_FullScreen,guiApp.videoWindow.isFullScreen,evFullScreen );
+    F=AddMenuCheckItem( window1, (const char*)full_xpm, Menu,MSGTR_MENU_FullScreen,guiApp.videoWindow.isFullScreen,evFullScreen + ( True << 16 ) );
   if ( !guiInfo.Playing )
    {
     gtk_widget_set_sensitive( H,FALSE );

gui/interface.c
@@ -389,7 +389,7 @@ int gui(int what, void *data)
 
         switch ((int)data) {
         case MP_CMD_VO_FULLSCREEN:
-            uiEventHandling(evFullScreen, 0);
+            uiEventHandling(evFullScreen, True);
             break;
 
         case MP_CMD_PLAY_TREE_STEP:
@@ -793,7 +793,7 @@ int gui(int what, void *data)
         }
 
         if (gtkLoadFullscreen ^ guiApp.videoWindow.isFullScreen)
-            uiEventHandling(evFullScreen, 0);
+            uiEventHandling(evFullScreen, True);
 
         if (guiWinID >= 0)
             wsMoveWindow(&guiApp.mainWindow, True, 0, guiInfo.VideoHeight);
@@ -875,7 +875,7 @@ int gui(int what, void *data)
                     wsVisibleWindow(&guiApp.videoWindow, wsShowWindow);
 
                 if (gtkLoadFullscreen ^ guiApp.videoWindow.isFullScreen)
-                    uiEventHandling(evFullScreen, 0);
+                    uiEventHandling(evFullScreen, False);
             } else {
                 wsVisibleWindow(&guiApp.videoWindow, wsHideWindow);
                 guiInfo.VideoWindow = False;

gui/ui/main.c
@@ -376,7 +376,7 @@ NoPause:
           uiFullScreen();
           if ( !guiApp.videoWindow.isFullScreen )
            {
-            wsResizeWindow( &guiApp.videoWindow, guiInfo.VideoWidth, guiInfo.VideoHeight );
+            wsResizeWindow( &guiApp.videoWindow, iparam ? guiInfo.VideoWidth : guiApp.video.width, iparam ? guiInfo.VideoHeight : guiApp.video.height );
             wsMoveWindow( &guiApp.videoWindow, False, guiApp.video.x, guiApp.video.y );
            }
          }