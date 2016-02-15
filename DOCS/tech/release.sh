d341c2c5f6e7826f00c32528b4249f8d9b939381
rtogni <rtogni@b3059339-0415-0410-9bf9-f77b7e298cf2>
2016-02-15 20:37:13 +0000

    Update release script for 1.3.x
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/branches/1.3@37737 b3059339-0415-0410-9bf9-f77b7e298cf2

--- DOCS/tech/release.sh
+++ DOCS/tech/release.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # will create checkouts and tarballs in the current dir
-ver=1.1
+branch=1.3
+ver=1.3.0_dummy
+ffver=3.0
 dst=MPlayer-$ver
-svnurl=svn://svn.mplayerhq.hu/mplayer/trunk
-#svnurl=svn://svn.mplayerhq.hu/mplayer/branches/$ver
+#svnurl=svn://svn.mplayerhq.hu/mplayer/trunk
+svnurl=svn://svn.mplayerhq.hu/mplayer/branches/$branch
 
 rm -rf $dst/ $dst-DOCS/ $dst.tar*
 
@@ -17,15 +19,14 @@ fi
 # create HTML docs
 cp -a $dst/ $dst-DOCS/
 pushd $dst-DOCS/
-mkdir -p ffmpeg/libavutil
-./configure --yasm='' --language=all
+./configure --disable-ffmpeg_a --disable-mencoder --disable-mplayer --yasm='' --language=all
 make html-chunked
 popd
 mv $dst-DOCS/DOCS/HTML $dst/DOCS
 rm -rf $dst-DOCS/
 
 # git archive unfortunately is refused
-git clone --depth 1 git://source.ffmpeg.org/ffmpeg.git $dst/ffmpeg
+git clone --depth 1 --branch release/$ffver git://source.ffmpeg.org/ffmpeg.git $dst/ffmpeg
 rm -rf $dst/.git* $dst/ffmpeg/.git*
 
 # create .tar.gz and .tar.xz files
@@ -38,5 +39,5 @@ md5sum $dst.tar.xz > $dst.tar.xz.md5
 sha1sum $dst.tar.xz > $dst.tar.xz.sha1
 md5sum $dst.tar.gz > $dst.tar.gz.md5
 sha1sum $dst.tar.gz > $dst.tar.gz.sha1
-gpg -a --detach-sign $dst.tar.xz
-gpg -a --detach-sign $dst.tar.gz
+#gpg -a --detach-sign $dst.tar.xz
+#gpg -a --detach-sign $dst.tar.gz
